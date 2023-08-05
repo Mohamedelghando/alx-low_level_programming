8eccc65f0cc993a82afe3616fc72aa71db5e4c9d
Mohamedelghando <mo.elghandour16@gmail.com>
2023-08-04 18:42:36 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,43 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
 
-/**
- * main - prints the minimum number of coins to
- * make change for an amount of money
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
- */
-int main(int argc, char *argv[])
-{
-	int num, j, result;
-	int coins[] = {25, 10, 5, 2, 1};
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	num = atoi(argv[1]);
-	result = 0;
-
-	if (num < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (j = 0; j < 5 && num >= 0; j++)
-	{
-		while (num >= coins[j])
-		{
-			result++;
-			num -= coins[j];
-		}
-	}
-
-	printf("%d\n", result);
-	return (0);
+int main() {
+    // Array to store the values of coins
+    int coins[] = {25, 10, 5, 1};
+    int numCoins = sizeof(coins) / sizeof(coins[0]);
+    
+    int amount, i;
+    int count = 0;
+    
+    printf("Enter the amount in cents: ");
+    scanf("%d", &amount);
+    
+    printf("Minimum number of coins needed:\n");
+    
+    for (i = 0; i < numCoins; i++) {
+        int coinCount = amount / coins[i];
+        amount = amount % coins[i];
+        count += coinCount;
+        
+        if (coinCount > 0) {
+            printf("%d coin(s) of %d cent(s)\n", coinCount, coins[i]);
+        }
+    }
+    
+    printf("Total number of coins: %d\n", count);
+    
+    return 0;
+}
